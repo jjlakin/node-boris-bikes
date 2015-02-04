7eff4bd227a94610bbbe27c9fd2215e1fa82effe
guspowell <augustuspowell@me.com>
2015-02-04 14:23:59 +0000

    done all dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,30 @@
 module.exports = function(grunt){
 
 	grunt.initConfig({
-		
+
+		pkg: grunt.file.readJSON('package.json'),
+
 		jasmine_node: {
 			all: ['spec/']
 		},
-		
+
 		jshint:{
 			all: ['Gruntfile.js', 'spec/**/*.js', 'src/**/*.js']
+		},
+
+		mocha_casperjs: {
+			options: {
+			},
+			files: {
+				src: ['test/**/*']
+			}
 		}
 
 	});
 
 	grunt.loadNpmTasks('grunt-jasmine-node');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.registerTask('default', ['jshint', 'jasmine_node']);
+	grunt.loadNpmTasks('grunt-mocha-casperjs');
+	grunt.registerTask('default', ['jshint', 'jasmine_node', 'mocha_casperjs']);
 
 };
